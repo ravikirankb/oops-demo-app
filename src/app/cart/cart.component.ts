bce2293e8adea41c5d270946582ab5768613fa81
ravikirankb <ravikirankb@gmail.com>
2023-11-05 11:20:55 +0530

    Move item from basket to cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Fruits } from '../models/fruits';
+import { SharedDataService } from '../services/shared-date-service.service'
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  loaded: string = "";
+  fruits:Array<Fruits>;
 
-  ngOnInit(): void {
+  constructor(public sharedDataService: SharedDataService) {
+    this.fruits = new Array<Fruits>();
+    this.sharedDataService.onAddToCart.subscribe({
+      next: (fruits: Fruits) => {
+        console.log(`Received message`);
+        if (fruits.fruits.length > 0) {
+           this.fruits.push(fruits);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+           this.loaded = "loaded";
+        }
+      }
+    })
   }
 
+  ngOnInit(): void {
+
+  }
 }
