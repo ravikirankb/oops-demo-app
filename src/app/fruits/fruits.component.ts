08258361ddd505dbd5d882c75d9e8fb85aa86e03
ravikirankb <ravikirankb@gmail.com>
2024-02-10 15:10:55 +0530

    ng build update, fruit image path update

--- src/app/fruits/fruits.component.ts
+++ src/app/fruits/fruits.component.ts
@@ -31,11 +31,11 @@ export class FruitsComponent implements OnInit, AfterViewInit{
 
   ngOnInit(): void {
     this.fruitData = ([
-      new FruitData("Apple", "appl", "/assets/static/Images/apple-svgrepo-com.svg"),
-      new FruitData("Banana", "ban", "/assets/static/Images/banana-svgrepo-com.svg"),
-      new FruitData("Mango", "mang", "/assets/static/Images/mango-svgrepo-com.svg"),
-      new FruitData("Orange", "orng", "/assets/static/Images/orange-svgrepo-com.svg"),
-      new FruitData("Pineapple", "pineap", "/assets/static/Images/pineapple-svgrepo-com.svg")
+      new FruitData("Apple", "appl", "assets/static/Images/apple-svgrepo-com.svg"),
+      new FruitData("Banana", "ban", "assets/static/Images/banana-svgrepo-com.svg"),
+      new FruitData("Mango", "mang", "assets/static/Images/mango-svgrepo-com.svg"),
+      new FruitData("Orange", "orng", "assets/static/Images/orange-svgrepo-com.svg"),
+      new FruitData("Pineapple", "pineap", "assets/static/Images/pineapple-svgrepo-com.svg")
     ]);
   }
 
